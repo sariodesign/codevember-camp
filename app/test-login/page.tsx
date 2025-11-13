83b08a6c74c140720c4e0eb906e4b09e5bf636b3
lucaradio24 <lukaabeck3@gmail.com>
2025-11-13 16:04:53 +0100

    feat(5): add logout action

--- app/test-login/page.tsx
+++ app/test-login/page.tsx
@@ -1,6 +1,22 @@
+import { redirect } from "next/dist/client/components/navigation"
+import { checkSession } from "../actions/checkSession"
+import { logout } from "../actions/logout"
+import { Button } from "@/components/ui/button"
+
+
 
 export default async function TestLogin() {
+
+  const { data } = await checkSession()
+
+  if (!data.session) {
+    redirect("/login")
+  }
+
     return (
-        <div>sono loggato!</div>
+        <div>sono loggato!
+
+            <Button onClick={logout}>Logout</Button>
+        </div>
     )
 }
