65d4a16fe318b684fe166369c6552c8c7c5bdfda
lucaradio24 <lukaabeck3@gmail.com>
2025-11-17 15:41:02 +0100

    fix: add default values to onboarding forms

--- components/forms/FocusTimeForm.tsx
+++ components/forms/FocusTimeForm.tsx
@@ -1,29 +1,39 @@
-'use client';
+"use client";
 
-import { useForm } from '@tanstack/react-form';
-import { Label } from '../ui/label';
-import { Input } from '../ui/input';
+import { useForm } from "@tanstack/react-form";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from '../ui/select';
-import { RadioGroup, RadioGroupItem } from '../ui/radio-group';
-import { Button } from '../ui/button';
+} from "../ui/select";
+import { RadioGroup, RadioGroupItem } from "../ui/radio-group";
+import { Button } from "../ui/button";
 
-interface Props {
-  onSubmit: (value: any) => void;
+interface FocusTime {
+  productiveTimeSlot: string;
+  focusTimeLength: string;
+  pauseTimeLength: string;
+  sessionsBeforeBreak: string;
 }
 
-export const FocusTimeForm = ({ onSubmit }: Props) => {
+interface Props {
+  onSubmit: (value: FocusTime) => void;
+  defaultValues?: FocusTime;
+}
+
+
+
+export const FocusTimeForm = ({ onSubmit, defaultValues }: Props) => {
   const form = useForm({
-    defaultValues: {
-      productiveTimeSlot: '',
-      focusTimeLength: '',
-      pauseTimeLength: '',
-      sessionsBeforeBreak: '',
+    defaultValues: defaultValues || {
+      productiveTimeSlot: "",
+      focusTimeLength: "",
+      pauseTimeLength: "",
+      sessionsBeforeBreak: "",
     },
     onSubmit: async ({ value }) => {
       onSubmit(value);
@@ -31,21 +41,21 @@ export const FocusTimeForm = ({ onSubmit }: Props) => {
   });
 
   return (
-    <div className='space-y-6'>
-      <h1 className='text-xl font-semibold'>Focus Time</h1>
+    <div className="space-y-6">
+      <h1 className="text-xl font-semibold">Focus Time</h1>
       <form
         onSubmit={(e) => {
           e.preventDefault();
           form.handleSubmit();
         }}
       >
-        <div className='space-y-4'>
+        <div className="space-y-4">
           <form.Field
-            name='productiveTimeSlot'
+            name="productiveTimeSlot"
             validators={{
               onChange: ({ value }) => {
                 if (!value) {
-                  return 'Quando sei più produttivo? è obbligatorio';
+                  return "Quando sei più produttivo? è obbligatorio";
                 }
                 return undefined;
               },
@@ -53,8 +63,8 @@ export const FocusTimeForm = ({ onSubmit }: Props) => {
           >
             {(field) => {
               return (
-                <div className='space-y-2'>
-                  <div className='flex items-start py-2'>
+                <div className="space-y-2">
+                  <div className="flex items-start py-2">
                     <Label htmlFor={field.name}>
                       In quale momento della giornata sei più produttivo?
                     </Label>
@@ -63,19 +73,19 @@ export const FocusTimeForm = ({ onSubmit }: Props) => {
                     value={field.state.value}
                     onValueChange={(value) => field.handleChange(value)}
                   >
-                    <SelectTrigger className='w-full'>
-                      <SelectValue placeholder='Seleziona un momento' />
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Seleziona un momento" />
                     </SelectTrigger>
                     <SelectContent>
-                      <SelectItem value='mattina'>Mattina</SelectItem>
-                      <SelectItem value='pomeriggio'>Pomeriggio</SelectItem>
-                      <SelectItem value='sera'>Sera</SelectItem>
-                      <SelectItem value='notte'>Notte</SelectItem>
+                      <SelectItem value="mattina">Mattina</SelectItem>
+                      <SelectItem value="pomeriggio">Pomeriggio</SelectItem>
+                      <SelectItem value="sera">Sera</SelectItem>
+                      <SelectItem value="notte">Notte</SelectItem>
                     </SelectContent>
                   </Select>
-                  {field.state.meta.errorMap['onChange'] && (
-                    <em className='text-red-700 dark:text-red-400 text-sm'>
-                      {field.state.meta.errorMap['onChange']}
+                  {field.state.meta.errorMap["onChange"] && (
+                    <em className="text-red-700 dark:text-red-400 text-sm">
+                      {field.state.meta.errorMap["onChange"]}
                     </em>
                   )}
                 </div>
@@ -83,11 +93,11 @@ export const FocusTimeForm = ({ onSubmit }: Props) => {
             }}
           </form.Field>
           <form.Field
-            name='focusTimeLength'
+            name="focusTimeLength"
             validators={{
               onChange: ({ value }) => {
                 if (!value) {
-                  return 'Quanto dovrebbero durare le tue sessioni di focus? è obbligatorio';
+                  return "Quanto dovrebbero durare le tue sessioni di focus? è obbligatorio";
                 }
                 return undefined;
               },
@@ -95,8 +105,8 @@ export const FocusTimeForm = ({ onSubmit }: Props) => {
           >
             {(field) => {
               return (
-                <div className='space-y-2'>
-                  <div className='flex items-start py-2'>
+                <div className="space-y-2">
+                  <div className="flex items-start py-2">
                     <Label htmlFor={field.name}>
                       Quanto dovrebbero durare le tue sessioni di focus?
                     </Label>
@@ -105,44 +115,44 @@ export const FocusTimeForm = ({ onSubmit }: Props) => {
                     value={field.state.value}
                     onValueChange={(value) => field.handleChange(value)}
                   >
-                    <div className='flex items-center gap-2'>
-                      <RadioGroupItem value='25' id='25' />
-                      <Label htmlFor='25'>25 min</Label>
+                    <div className="flex items-center gap-2">
+                      <RadioGroupItem value="25" id="25" />
+                      <Label htmlFor="25">25 min</Label>
                     </div>
-                    <div className='flex items-center gap-2'>
-                      <RadioGroupItem value='50' id='50' />
-                      <Label htmlFor='50'>50 min</Label>
+                    <div className="flex items-center gap-2">
+                      <RadioGroupItem value="50" id="50" />
+                      <Label htmlFor="50">50 min</Label>
                     </div>
-                    <div className='flex items-center gap-2'>
-                      <RadioGroupItem value='90' id='90' />
-                      <Label htmlFor='90'>90 min</Label>
+                    <div className="flex items-center gap-2">
+                      <RadioGroupItem value="90" id="90" />
+                      <Label htmlFor="90">90 min</Label>
                     </div>
-                    <div className='flex items-center gap-2'>
-                      <RadioGroupItem value='custom' id='custom' />
+                    <div className="flex items-center gap-2">
+                      <RadioGroupItem value="custom" id="custom" />
                       <Input
-                        type='number'
-                        placeholder='Inserisci la durata'
+                        type="number"
+                        placeholder="Inserisci la durata"
                         value={
                           field.state.value &&
-                          !['25', '50', '90', 'custom'].includes(
+                          !["25", "50", "90", "custom"].includes(
                             field.state.value
                           )
                             ? field.state.value
-                            : ''
+                            : ""
                         }
                         onChange={(e) => {
                           field.handleChange(e.target.value);
                         }}
                         disabled={
                           !field.state.value ||
-                          ['25', '50', '90'].includes(field.state.value)
+                          ["25", "50", "90"].includes(field.state.value)
                         }
                       />
                     </div>
                   </RadioGroup>
-                  {field.state.meta.errorMap['onChange'] && (
-                    <em className='text-red-700 dark:text-red-400 text-sm'>
-                      {field.state.meta.errorMap['onChange']}
+                  {field.state.meta.errorMap["onChange"] && (
+                    <em className="text-red-700 dark:text-red-400 text-sm">
+                      {field.state.meta.errorMap["onChange"]}
                     </em>
                   )}
                 </div>
@@ -150,11 +160,11 @@ export const FocusTimeForm = ({ onSubmit }: Props) => {
             }}
           </form.Field>
           <form.Field
-            name='pauseTimeLength'
+            name="pauseTimeLength"
             validators={{
               onChange: ({ value }) => {
                 if (!value) {
-                  return 'Che tipo di pause preferisci? è obbligatorio';
+                  return "Che tipo di pause preferisci? è obbligatorio";
                 }
                 return undefined;
               },
@@ -162,8 +172,8 @@ export const FocusTimeForm = ({ onSubmit }: Props) => {
           >
             {(field) => {
               return (
-                <div className='space-y-2'>
-                  <div className='flex items-start py-2'>
+                <div className="space-y-2">
+                  <div className="flex items-start py-2">
                     <Label htmlFor={field.name}>
                       Che tipo di pause preferisci?
                     </Label>
@@ -172,18 +182,18 @@ export const FocusTimeForm = ({ onSubmit }: Props) => {
                     value={field.state.value}
                     onValueChange={(value) => field.handleChange(value)}
                   >
-                    <SelectTrigger className='w-full'>
-                      <SelectValue placeholder='Seleziona la durata' />
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Seleziona la durata" />
                     </SelectTrigger>
                     <SelectContent>
-                      <SelectItem value='5min'>5 min</SelectItem>
-                      <SelectItem value='15min'>15 min</SelectItem>
-                      <SelectItem value='30min'>30 min</SelectItem>
+                      <SelectItem value="5min">5 min</SelectItem>
+                      <SelectItem value="15min">15 min</SelectItem>
+                      <SelectItem value="30min">30 min</SelectItem>
                     </SelectContent>
                   </Select>
-                  {field.state.meta.errorMap['onChange'] && (
-                    <em className='text-red-700 dark:text-red-400 text-sm'>
-                      {field.state.meta.errorMap['onChange']}
+                  {field.state.meta.errorMap["onChange"] && (
+                    <em className="text-red-700 dark:text-red-400 text-sm">
+                      {field.state.meta.errorMap["onChange"]}
                     </em>
                   )}
                 </div>
@@ -191,11 +201,11 @@ export const FocusTimeForm = ({ onSubmit }: Props) => {
             }}
           </form.Field>
           <form.Field
-            name='sessionsBeforeBreak'
+            name="sessionsBeforeBreak"
             validators={{
               onChange: ({ value }) => {
                 if (!value) {
-                  return 'Quante sessioni di focus vuoi fare prima di una pausa? è obbligatorio';
+                  return "Quante sessioni di focus vuoi fare prima di una pausa? è obbligatorio";
                 }
                 return undefined;
               },
@@ -203,8 +213,8 @@ export const FocusTimeForm = ({ onSubmit }: Props) => {
           >
             {(field) => {
               return (
-                <div className='space-y-2'>
-                  <div className='flex items-start py-2'>
+                <div className="space-y-2">
+                  <div className="flex items-start py-2">
                     <Label htmlFor={field.name}>
                       Quante sessioni di focus vuoi fare prima di una pausa?
                     </Label>
@@ -213,29 +223,29 @@ export const FocusTimeForm = ({ onSubmit }: Props) => {
                     value={field.state.value}
                     onValueChange={(value) => field.handleChange(value)}
                   >
-                    <div className='flex items-center gap-2'>
-                      <RadioGroupItem value='1' id='1' />
-                      <Label htmlFor='1'>1</Label>
+                    <div className="flex items-center gap-2">
+                      <RadioGroupItem value="1" id="1" />
+                      <Label htmlFor="1">1</Label>
                     </div>
-                    <div className='flex items-center gap-2'>
-                      <RadioGroupItem value='2' id='2' />
-                      <Label htmlFor='2'>2</Label>
+                    <div className="flex items-center gap-2">
+                      <RadioGroupItem value="2" id="2" />
+                      <Label htmlFor="2">2</Label>
                     </div>
-                    <div className='flex items-center gap-2'>
-                      <RadioGroupItem value='3' id='3' />
-                      <Label htmlFor='3'>3 o più</Label>
+                    <div className="flex items-center gap-2">
+                      <RadioGroupItem value="3" id="3" />
+                      <Label htmlFor="3">3 o più</Label>
                     </div>
                   </RadioGroup>
-                  {field.state.meta.errorMap['onChange'] && (
-                    <em className='text-red-700 dark:text-red-400 text-sm'>
-                      {field.state.meta.errorMap['onChange']}
+                  {field.state.meta.errorMap["onChange"] && (
+                    <em className="text-red-700 dark:text-red-400 text-sm">
+                      {field.state.meta.errorMap["onChange"]}
                     </em>
                   )}
                 </div>
               );
             }}
           </form.Field>
-          <Button type='submit' className='w-full'>
+          <Button type="submit" className="w-full">
             Continua
           </Button>
         </div>
