6fddefbdfcfb04bc3fec8646e6ade93058aba71a
Manuel Desole <desole.manuel@gmail.com>
2025-11-25 20:25:01 +0100

    feat(30): aggiungere schemi di validazione per il processo di onboarding utente

---
+++ lib/validations/onboarding.ts
@@ -0,0 +1,29 @@
+import { z } from "zod";
+
+export const projectSchema = z.object({
+    name: z.string().min(1, "Il nome del progetto è obbligatorio"),
+    priority: z.string().min(1, "La priorità è obbligatoria"),
+});
+
+export const focusTimeFormSchema = z.object({
+    productiveTimeSlot: z.string().min(1, "Seleziona un momento della giornata"),
+    focusTimeLength: z.string().min(1, "Seleziona la durata della sessione"),
+    pauseTimeLength: z.string().min(1, "Seleziona la durata della pausa"),
+    sessionsBeforeBreak: z.string().min(1, "Seleziona il numero di sessioni prima della pausa"),
+})
+
+export const pojectsFormSchema = z.object({
+    numberOfProjects: z.number().min(1, "Aggiungere almeno un progetto"),
+    projects: z.array(projectSchema),
+})
+
+export const onboardingSchema = z.object({
+    focusTime: focusTimeFormSchema,
+    projects: pojectsFormSchema,
+});
+
+
+export type Project = z.infer<typeof projectSchema>
+export type onboarding = z.infer<typeof onboardingSchema>;
+export type ProjectForm = z.infer<typeof pojectsFormSchema>;
+export type FocusTimeForm = z.infer<typeof focusTimeFormSchema>;
