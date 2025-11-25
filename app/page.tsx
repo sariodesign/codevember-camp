6b608f6365b7d3604d4f7339b5a8a17c385b9a2c
Manuel Desole <desole.manuel@gmail.com>
2025-11-25 17:28:06 +0100

    feat(30): implement user onboarding flow with redirection based on completion status

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,24 @@
 import { getCurrentUser } from "./actions/getCurrentUser";
 import { redirect } from "next/navigation";
 import LoginPage from "./login/page";
+import { SupabaseClient } from "@supabase/supabase-js";
+import { Database } from "@/utils/supabase/database.types";
+import { createClient } from "@/utils/supabase/server";
 
 export default async function Home() {
   const { data } = await getCurrentUser()
-
-  if (data.user) {
-    redirect("/dashboard")
+  if (!data.user?.id) {
+    return (
+      <LoginPage />
+    );
   }
 
-  return (
-    <LoginPage />
-  );
+  const supabase: SupabaseClient<Database> = await createClient()
+  const { data: userOnboarding } = await supabase.from("onboardings").select("*").eq("user_id", data.user?.id).single()
+
+  if (data.user && userOnboarding?.completed_at) {
+    redirect("/dashboard")
+  } else {
+    redirect("/onboarding")
+  }
 }
